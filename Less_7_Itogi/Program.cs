51010b20d7383d2d161cbcdb14e9988b1f649695
Alexandr Uvaev <alex211275@yandex.ru>
2023-10-07 14:51:38 +0300

    Добавьте файлы проекта.

---
+++ Less_7_Itogi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Less_7_Itogi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
